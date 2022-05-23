5eab8df35cd1a35bb0b7083b963f6521fba77fe6
PhuongBlu <phamnhatphuong.work@gmail.com>
2022-05-23 14:26:55 +0700

    fix error "fileManager" is null

--- src/main/java/org/blu/blib/bLib.java
+++ src/main/java/org/blu/blib/bLib.java
@@ -2,19 +2,24 @@ package org.blu.blib;
 
 import org.blu.blib.File.FileManager;
 import org.blu.blib.Util.Text;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class bLib extends JavaPlugin {
 
+    private final FileManager fileManager = new FileManager();
     private Text text;
-    private FileManager fileManager;
 
     @Override
     public void onEnable() {
         final long currentTimeMillis = System.currentTimeMillis();
+        // bStats
+        int pluginId = 15271;
+        Metrics metrics = new Metrics(this, pluginId);
 
+        //setup File
         fileManager.setupFile();
 
-        text.sendInfoLog("Completed in " + (System.currentTimeMillis() - currentTimeMillis) + " ms.");
+        Text.sendInfoLog("Completed in " + (System.currentTimeMillis() - currentTimeMillis) + " ms.");
     }
 }
