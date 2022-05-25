52c43cc1e97df96c76a658c715175fe85e11f288
PhuongBlu <phamnhatphuong.work@gmail.com>
2022-05-25 14:32:37 +0700

    recreate commandCore

--- src/main/java/org/blu/blib/Command/CommandCore.java
+++ src/main/java/org/blu/blib/Command/CommandCore.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public abstract class CommandCore extends Command {
 
+
     protected CommandCore(String name) {
         super(name);
     }
@@ -16,14 +17,20 @@ public abstract class CommandCore extends Command {
         super(name, description, usageMessage, aliases);
     }
 
-    public static void registerCommand(final String fallback, final CommandCore command) {
+    public static void registerCommand(final String name, final CommandCore command) {
         try {
             final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
             bukkitCommandMap.setAccessible(true);
-            final CommandMap commandMap = (CommandMap)Bukkit.getServer().getClass().getDeclaredField("commandMap").get(Bukkit.getServer());
-            commandMap.register(fallback, command);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
+            final CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
+            commandMap.register(name, (org.bukkit.command.Command)command);
+            if (commandMap instanceof CommandMap) {
+                commandMap.register(name, command);
+            }
+        } catch (IllegalAccessException | IllegalArgumentException | NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
         }
     }
+
+    @Override
+    public abstract boolean execute(CommandSender commandSender, String label, String[] args);
 }
